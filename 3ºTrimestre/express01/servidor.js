936a503fff78c6b048386fa7537b2f1710f84d4f
babilonia2milantesdeCristo <rodrigo.filho20@escola.pr.gov.br>
2026-09-01 11:31:32 +0000

    Iniciando configuração para Express01

---
+++ 3ºTrimestre/express01/servidor.js
@@ -0,0 +1,13 @@
+// importação moderna com ESM (import)
+import express from 'express'
+
+const app = rexpress ()
+
+app.get('/', (res, res) => {
+    res.send('Home Ok!')
+})
+app.get('/Negresco', (res, res) => {
+    res.send('Que bolacha GOSTOSA!!!!!!!')
+})
+
+app.listen(3000, () => {console.log('Servidor está viva!')})
